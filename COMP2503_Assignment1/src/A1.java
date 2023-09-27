3b20f6f91f19da90f4628f614d54307d35700b4e
alandeta <aland271@mtroyal.ca>
2023-09-27 08:53:21 -0600

    read input, setters getters added
    read input while loop, word check started, setters getters, Avenger
    constructor created, comments added back

--- COMP2503_Assignment1/src/A1.java
+++ COMP2503_Assignment1/src/A1.java
@@ -1,6 +1,16 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * COMP 2503 Fall 2023 Assignment 1
+ * 
+ * This program reads a input stream and keeps track of the statistics for avengers
+ * mentioned by name, alias, or performer's last name.
+ *
+ * @author Maryam Elahi
+ * @date Fall 2023
+ */
+
 public class A1 {
 	
 	public String[][] avengerRoster = {
@@ -27,30 +37,78 @@ public class A1 {
 		printResults();
 	}
 		
-		
+	/**
+	 * read the input stream and keep track  
+	 * how many times avengers are mentioned by alias or last name or performer's last name.
+	 */
 	private void readInput() {
-		String textInput = input.next();
-		textInput = textInput.trim().toLowerCase().replaceAll("'"," ").replaceAll("[^a-zA-Z\\d]","");
+		
+		/*
+		In a loop, while the scanner object has not reached end of stream,
+		 	- read a word.
+		 	- clean up the word
+		    - if the word is not empty, add the word count. 
+		    - Check if the word is either an avenger alias or last name, or performer last name then
+				- Create a new avenger object with the corresponding alias and last name and performer last name.
+				- if this avenger has already been mentioned, increase the corresponding frequency count for the object already in the list.
+				- if this avenger has not been mentioned before, add the newly created avenger to the list, remember to update the corresponding frequency.
+		*/ 
+		
+		while (input.hasNext()) {
+			String word = input.next();
+			word = word.trim().toLowerCase().replaceAll("'"," ").replaceAll("[^a-zA-Z\\d]","");
+			if (word.length() > 0) {
+				totalwordcount++;
+			}
+			for (int r = 0; r < avengerRoster.length; r++) {
+				for (int c = 0; c < avengerRoster[r].length; c++) {
+					if (r == 0) {
+						
+					}
+				}
+			}
+		}
 		
 	}
-
-	private void printResults() {
-		System.out.println("Total number of words:" + totalwordcount);
-		System.out.println("Number of Avengers Mentioned:" + avengersArrayList.size());
-		System.out.println();
-		
-		
-	}
-
-
-
-
-		{
-			
-			
-			
-		
 	
+	/*
+	 * Create helper functions as needed
+	 */
+	
+	/**
+	 * print the results
+	 */
+	private void printResults() {
+		/*
+		 * Make sure your format matches the sample output exactly. 
+		 * No extra empty lines, or text. Use the diff command to check if your output matches the sample outputs.
+		 */
+		System.out.println("Total number of words: " + totalwordcount);
+		System.out.println("Number of Avengers Mentioned: " + avengersArrayList.size());
+		System.out.println();
 
-}
+		// Print all ordered by appearance
+		System.out.println("All avengers in the order they appeared in the input stream:");
+		// Todo: Print the list of avengers in the order they appeared in the input
+		// Make sure you follow the formatting example in the sample output
+		System.out.println();
+
+		// Print all ordered by AvengerComparatorFreqDesc
+		System.out.println("Top " + topN + " most popular avengers:");
+		// Todo: Print the topN most popular avengers, see the instructions for tie breaking
+		// Make sure you follow the formatting example in the sample output
+		System.out.println();
+
+		// Print top n ordered by AvengerPerformerComparatorFreqDesc
+		System.out.println("Top " + topN + " most popular performers:");
+		// Todo: Print the topN most popular performers, see the instructions for tie breaking
+		// Make sure you follow the formatting example in the sample output	
+		System.out.println();
+
+		// Print all ordered by alias alphabetically
+		System.out.println("All mentioned avengers in alphabetical order:");
+		// Todo: Print the list of avengers in alphabetical order
+		// Make sure you follow the formatting example in the sample output
+		System.out.println();
+	}
 }
--- COMP2503_Assignment1/src/Avenger.java
+++ COMP2503_Assignment1/src/Avenger.java
@@ -4,37 +4,73 @@ public class Avenger implements Comparable<Avenger>{
 	private String heroName;
 	private String heroAlias;
 	private String performer;
+	
 	private int nameFreq;
 	private int aliasFreq;
 	private int performerFreq;
+	
+	public Avenger() {
+		nameFreq++; // change to int=0?
+		aliasFreq++; // change to int=0?
+		performerFreq++; //change to into=0?
+		heroAlias = "";
+		heroName = "";
+		performer = "";
+		
+	}
 	
 	@Override
 	public int compareTo(Avenger o) {
 		return 0;
 	}
 
-	public String getHeroName() { return heroName; }
-	public void setHeroName(String heroName) { this.heroName = heroName; }
+	public String getHeroAlias() {
+		return heroAlias;
+	}
 
-	public String getHeroAlias() { return heroAlias; }
-	public void setHeroAlias(String heroAlias) { this.heroAlias = heroAlias; }
-	
-	public String getPerformer() { return performer; }
-	public void setPerformer(String performer) { this.performer = performer; }
-	
-	public int getNameFreq() { return nameFreq; }
-	public void setNameFreq(int nameFreq) { this.nameFreq = nameFreq; }
-	
-	public int getAliasFreq() { return aliasFreq; }
-	public void setAliasFreq(int aliasFreq) { this.aliasFreq = aliasFreq; }
-	
-	public int getPerformerFreq() { return performerFreq;}
-	public void setPerformerFreq(int performerFreq) { this.performerFreq = performerFreq; }
+	public void setHeroAlias(String heroAlias) {
+		this.heroAlias = heroAlias;
+	}
 
-	public String toString() { return heroAlias + "aka" + heroName + "performed by" + 
-								performer + "mentioned" + "(n:" + nameFreq + " + a: " + 
-								aliasFreq + " + p: " + performerFreq + ")" + "time(s)"; }
-	
+	public String getHeroName() {
+		return heroName;
+	}
+
+	public void setHeroName(String heroName) {
+		this.heroName = heroName;
+	}
+
+	public String getPerformer() {
+		return performer;
+	}
+
+	public void setPerformer(String performer) {
+		this.performer = performer;
+	}
+
+	public int getNameFreq() {
+		return nameFreq;
+	}
+
+	public void setNameFreq(int nameFreq) {
+		this.nameFreq = nameFreq;
+	}
+
+	public int getAliasFreq() {
+		return aliasFreq;
+	}
+
+	public void setAliasFreq(int aliasFreq) {
+		this.aliasFreq = aliasFreq;
+	}
+
+	public int getPerformerFreq() {
+		return performerFreq;
+	}
+
+	public void setPerformerFreq(int performerFreq) {
+		this.performerFreq = performerFreq;
+	}
 	
 	
 }
